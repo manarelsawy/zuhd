43de032b739216387a88d71e07295aa6f1467bb4
manar-s01 <manar.s@ergasti.com.eg>
2025-04-20 23:08:00 +0200

    home page commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,13 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./components/header/header.component";
 import { NavComponent } from "./components/nav/nav.component";
 import { HomeComponent } from "./components/home/home.component";
+import { ServicesComponent } from "./components/services/services.component";
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, NavComponent, HomeComponent],
+  imports: [RouterOutlet, HeaderComponent, NavComponent, HomeComponent, ServicesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
